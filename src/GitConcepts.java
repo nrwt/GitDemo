165404c656c7f54c14b5e82e87c43fe422dcff12
nandini <nndn.rwt@gmail.com>
2023-04-17 22:26:58 +0100

    demo guy

---
+++ src/GitConcepts.java
@@ -0,0 +1,18 @@
+
+public class GitConcepts {
+	
+public void getData() {
+		
+		System.out.println("Hello World");
+		
+	}
+
+	public static void main(String[] args) {
+		
+		GitConcepts gc = new GitConcepts();
+		gc.getData();
+		// TODO Auto-generated method stub
+
+	}
+
+}
